3775cf1bb23b9c9af487bc27e3ef84e2baf67796
Maheer <86462580+okmaheer@users.noreply.github.com>
2022-08-04 12:04:39 +0500

    Update user.js

--- models/user.js
+++ models/user.js
@@ -11,20 +11,13 @@ const userSchema = new Schema({
     type: String,
     required: true
   },
-  resetToken: String,
-  resetTokenExpiration: Date,
-  cart: {
-    items: [
-      {
-        productId: {
-          type: Schema.Types.ObjectId,
-          ref: 'Product',
-          required: true
-        },
-        quantity: { type: Number, required: true }
-      }
-    ]
-  }
+  name: {
+    type: String,
+    required: true
+  },
+
+      
+  
 });
 
 userSchema.methods.addToCart = function(product) {
